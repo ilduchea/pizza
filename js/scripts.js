f43f8d58f0800b93ac0af6b9001abfcb3d244dcb
ilduchea <ilduchea@gmail.com>
2017-04-07 09:13:51 -0800

    add base html. add base js objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
 //Buisness Logic
+function Order() {
+  this.pizza = [];
+  this.totalCost;
+}
+
+function Pizza() {
+  this.size;
+  this.crust;
+  this.suace;
+  this.toppings = [];
+  this.cost;
+}
+
+var pizzaSize = {
+  small: 5,
+  medium: 7,
+  large: 9,
+  xLarge: 11,
+}
+
 
 //UI Logic
 $(function(){
