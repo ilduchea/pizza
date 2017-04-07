875d31fd11ae370935b716f8f704057970c18b86
ilduchea <ilduchea@gmail.com>
2017-04-07 09:26:44 -0800

    add js methods for finding the subtotal and the total price.

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,15 @@ var pizzaSize = {
   xLarge: 11,
 }
 
+Pizza.prototype.subtotal = function (size, toppins) {
+  var subtotal = size + toppings;
+  return subtotal;
+};
+
+Order.prototype.orderPrice = function (array) {
+  var price = array.reduce((a, b) => a + b, 0);
+  return price;
+};
 
 //UI Logic
 $(function(){
